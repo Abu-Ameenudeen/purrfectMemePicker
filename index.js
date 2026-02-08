106b143148c5155497691c31cb5986bc5af1f7fa
Abu Ameen <113390687+Abu-Ameenudeen@users.noreply.github.com>
2026-02-08 22:19:30 +0530

    Update the emotionRadios eventListener
    - removed the anonymous function from the emotionRadios eventListener.
    - Added new highlightCheckedOption to reflect the accent color change.

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import { catsData } from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 
-emotionRadios.addEventListener('change', function(event) {
-    console.log(event.target.id)
-})
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e) {
+    document.getElementById(e.target.id).classList.add('highlight')
+}
 
 function getEmotionsArray(cats){
     const emotionsArray = []
