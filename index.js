6a1aa0979383ef9e14dc502afd06726011eab77b
Abu Ameen <113390687+Abu-Ameenudeen@users.noreply.github.com>
2026-02-10 08:39:35 +0530

    Add getMatchingCatsArray
    - To return a Matching array of objects with Matching emotions
    - Add event listener for the get img btn
    - Add gif option to verify the required objects

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import { catsData } from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
+getImageBtn.addEventListener('click', getMatchingCatsArray)
 
 function highlightCheckedOption(e) {
     const radios = document.getElementsByClassName('radio')
@@ -23,6 +26,24 @@ function getEmotionsArray(cats){
         }
     }
     return emotionsArray
+}
+
+function getMatchingCatsArray(){     
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        const isGif = gifsOnlyOption.checked
+        
+        const matchingCatsArray = catsData.filter(function(cat){
+            
+            if(isGif){
+                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+            }
+            else{
+                return cat.emotionTags.includes(selectedEmotion)
+            }            
+        })
+        console.log( matchingCatsArray )
+    }   
 }
 
 function renderEmotionsRadios(cats){
